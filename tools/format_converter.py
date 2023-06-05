c92de61c6bd4a7a7761f7df0af70834092ac29ab
Martin-Masson <martinmasson03@gmail.com>
2023-06-05 19:17:49 +0200

    fixed tokenization

--- tools/format_converter.py
+++ tools/format_converter.py
@@ -1,93 +1,118 @@
-from typing import Dict, List, Tuple, TypedDict
+from __future__ import annotations
 
 from spacy.lang.en import English
-from spacy.util import compile_prefix_regex, compile_suffix_regex, compile_infix_regex
-from spacy.symbols import ORTH
+from spacy.tokenizer import Tokenizer
+from spacy import util
 import os
 import argparse
-
-from pprint import pprint
 
 
 class FormatConvertor:
     def __init__(self, input_dir: str, output_file: str) -> None:
+        """Initializes the instance based on the input directory and output file names.
+
+        Args:
+          input_dir: The relative path of the directory where the BRAT
+            annotated files are located.
+          output_file: The relative path of the output CoNLL file.
+        """
         self.input_dir = input_dir
         self.output_file = output_file
 
-    def get_file_pairs(self) -> List[Tuple[str, str]]:
+    def get_file_pairs(self) -> list[tuple[str, str]]:
+        """Pairs the ann and txt files of the input folder.
+
+        Returns:
+            A list of tuples (*.ann, *.txt), where *.ann is the relative
+              path of the BRAT annotated file and *.txt is the relative
+              path of its corresponding text file.
+        """
         file_pairs = []
         files = os.listdir(self.input_dir)
         annotation_files = sorted([file for file in files if file.endswith(".ann")])
-
+        # The folder is assumed to contain *.ann and *.txt where both files
+        # of a pair have the same name
         for ann_file in annotation_files:
             txt_file = ann_file.replace(".ann", ".txt")
             if txt_file in files:
                 file_pairs.append(
-                    (os.path.join(self.input_dir, ann_file), os.path.join(self.input_dir, txt_file))
+                    (os.path.join(self.input_dir, ann_file),
+                     os.path.join(self.input_dir, txt_file))
                 )
             else:
-                raise (f"{ann_file} does not have a corresponding text file.")
-        
+                raise f"{ann_file} does not have a corresponding text file."
+
         return file_pairs
 
-    def get_annotations(self, ann_file: str) -> List[TypedDict]:
-        annotations = []
+    @staticmethod
+    def get_annotations(ann_file: str) -> list[dict[str, str | int]]:
+        """Reads the useful annotations of the input BRAT file.
 
+        Args:
+            ann_file: The relative path of the BRAT annotated file.
+
+        Returns:
+            A list of dicts each mapping a line's tag, start and end to
+            their corresponding value.
+        """
+        annotations = []
+        # Read each line of the annotation file to a dictionary
         with open(ann_file, 'r') as f:
             for line in f:
                 if line != "\n":
-                    file_annotations = {}
+                    line_annotations = {}
                     splits = line.split()
-                    file_annotations["ner_tag"] = splits[1].lower()
-                    file_annotations["start"] = int(splits[2])
-                    file_annotations["end"] = int(splits[3])
-                    file_annotations["text"] = ' '.join(splits[4:])
-                    annotations.append(file_annotations)
-
+                    line_annotations["ner_tag"] = splits[1].lower()
+                    line_annotations["start"] = int(splits[2])
+                    line_annotations["end"] = int(splits[3])
+                    annotations.append(line_annotations)
+        # Sorts the annotations by start position to be able to loop through
+        # the text while iterating over the list
         annotations.sort(key=lambda x: x["start"])
         return annotations
 
-    def get_text(self, txt_file: str) -> str:
+    @staticmethod
+    def get_text(txt_file: str) -> str:
+        """Returns the content of the input text file as a string."""
         with open(txt_file, 'r') as f:
             text = f.read()
 
         return text
-    
-    def convert(self) -> None:
+
+    @staticmethod
+    def make_tokenizer() -> Tokenizer:
+        """Returns a spacy tokenizer with special rule changes for the task."""
         nlp = English()
         tokenizer = nlp.tokenizer
 
-        special_case = [{ORTH: "B"}, {ORTH: "."}]
-        tokenizer.add_special_case("B.", special_case)
+        prefixes = nlp.Defaults.prefixes + [r"(\d+(\.\d+)?)"]
+        suffixes = nlp.Defaults.suffixes + [r"(\d+(\.\d+)?)", r"\."]
+        infixes = nlp.Defaults.infixes + [r"(\d+(\.\d+)?)", r"--", r"\(", r"\)", r"/"]
 
-        prefixes = nlp.Defaults.prefixes #+ [r'-', r'±', r'≈']
-        suffixes = nlp.Defaults.suffixes #+ [r')', r'(?<=B)\.']
-        infixes = nlp.Defaults.infixes + [r'±', r'\+/-', r'-/\+', r'=', r'/(?=\d)', r':', r'--', r'\(', r'\)'] #'vs.' 
-        prefix_regex = compile_prefix_regex(prefixes)
-        suffix_regex = compile_suffix_regex(suffixes)
-        infix_regex = compile_infix_regex(infixes)
+        prefix_regex = util.compile_prefix_regex(prefixes)
+        suffix_regex = util.compile_suffix_regex(suffixes)
+        infix_regex = util.compile_infix_regex(infixes)
 
         tokenizer.prefix_search = prefix_regex.search
         tokenizer.suffix_search = suffix_regex.search
         tokenizer.infix_finditer = infix_regex.finditer
 
+        rules = tokenizer.rules.items()
+        tokenizer.rules = {k: v for k, v in rules if k not in ['):', '8)']}
+
+        return tokenizer
+
+    def convert(self) -> None:
+        """Loops over all the file pairs and write each token and its label to the output file."""
+        tokenizer = self.make_tokenizer()
+
         with open(self.output_file, 'w') as f:
             for ann_file, txt_file in self.get_file_pairs():
-
-                BUGED = [
-                    "brat_annotations/15023242.ann",
-                    "brat_annotations/16520033.ann",
-                    "brat_annotations/12796608.ann",
-                    "brat_annotations/17785705.ann",
-                ]
-                if ann_file in BUGED:
-                    continue
-
                 annotations = self.get_annotations(ann_file)
                 tokens = tokenizer(self.get_text(txt_file))
                 len_annotations = len(annotations)
 
-                annotation_idx = 0
+                annotation_idx = 0  # The index of annotation within annotations
                 tag_start = annotations[annotation_idx]["start"]
                 tag_end = annotations[annotation_idx]["end"]
                 tag_span = tokens.char_span(tag_start, tag_end)
@@ -96,19 +121,16 @@ class FormatConvertor:
                         f.write(f"{token} O\n")
                     else:
                         ner_tag = annotations[annotation_idx]["ner_tag"]
+                        # Whether the token is at the beginning or in the middle of the tagged text
                         pos = "B" if token == tag_span[0] else "I"
                         f.write(f"{token} {pos}-{ner_tag}\n")
+                        # If the token is the last of tagged text
                         if token == tag_span[-1]:
                             annotation_idx += 1
                             if annotation_idx < len_annotations:
                                 tag_start = annotations[annotation_idx]["start"]
                                 tag_end = annotations[annotation_idx]["end"]
                                 tag_span = tokens.char_span(tag_start, tag_end)
-                                if tag_span is None:
-                                    pprint(annotations); print()
-                                    print(f"TEXT : {tokens.text}"); print()
-                                    print(f"TOKENS : {[t.text for t in tokens]}"); print()
-                                    print(f"In {ann_file} : token '{token}' at {tag_start}-{tag_end}, {tag_span}"); print()
 
                 f.write("\n")
 
